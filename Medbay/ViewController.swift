7d26a096ba627f64dd95bffe072f44e44ad04527
Wiljay <wiljay.flores@gmail.com>
2018-11-30 05:36:46 -0500

    First commit

---
+++ Medbay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Medbay
+//
+//  Created by Wiljay Flores on 2018-11-30.
+//  Copyright © 2018 wiljay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
